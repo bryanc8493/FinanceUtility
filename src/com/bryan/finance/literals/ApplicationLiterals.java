e151d0a8fdd4124e3d49876fdcba58535692ed87
bryanc8493 <bryanc8493@gmail.com>
2018-04-02 22:25:05 -0500

    chore: minor breaking change

--- src/com/bryan/finance/literals/ApplicationLiterals.java
+++ src/com/bryan/finance/literals/ApplicationLiterals.java
@@ -29,7 +29,7 @@ public abstract class ApplicationLiterals {
 	public static final String SPACE = " ";
 
 	public static final String APP_TITLE = "Finance Utility";
-	public static final String VERSION = "1.1.2";
+	public static final String VERSION = "1.2.0";
 
 	private static final String ROOT_PASSWORD = "RootPassword";
 	private static final String ENCRYPTION_KEY = "EncryptionKey";
